6ed69c17dc5cee4dd6c0e8182ce34da1daafe777
kaito <gn00386614@gmail.com>
2019-10-30 15:54:36 +0800

    1st

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <router-link class="nav-link" :to="'/'">Home</router-link>       
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="'hello2'">hello2</router-link>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+/* #app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ src/components/HelloWorld2.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="hello container">
+    <div class="input-group mb-3 mt-3">
+      <input type="text" class="form-control" v-model="message" placeholder="輸入訊息" />
+      <div class="input-group-append">
+        <button
+          class="btn btn-outline-secondary"
+          @click="submitMessage()"
+          type="button"
+          id="button-addon2"
+        >送出</button>
+      </div>
+    </div>
+    <div class="media mt-3" v-for="item in messages">
+      <img
+        v-if="item.user==user"
+        src="https://picsum.photos/id/237/100/100"
+        class="align-self-start mr-3"
+        alt="..."
+      />
+      <div class="media-body">
+        <h5 class="mt-0">{{item.user}}</h5>
+        <p>{{item.message}}</p>
+      </div>
+      <img
+        v-if="item.user!=user"
+        src="https://picsum.photos/id/1062/100/100"
+        class="ml-3"
+        alt="..."
+      />
+    </div>
+    <!-- <div class="media">
+      <div class="media-body">
+        <h5 class="mt-0 mb-1">Media object</h5>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </div>
+      <img src="https://picsum.photos/id/1062/100/100" class="ml-3" alt="..." />
+    </div>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      user: "Casper",
+      message: "",
+      messages: [
+        {
+          user: "Casper",
+          message: "Hello 你好"
+        },
+        {
+          user: "卡斯楚",
+          message: "不好 FuckYou"
+        }
+      ]
+    };
+  },
+  methods: {
+    submitMessage() {
+      let self = this;
+      self.messages.unshift({
+        user:self.user,
+        message:self.message
+      });
+      self.message=''
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
